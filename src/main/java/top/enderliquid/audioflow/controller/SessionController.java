58bdc9b4766af3954f0e9660db2167757c5cfae7
EnderLiquid <3546817839@qq.com>
2026-02-19 16:35:59 +0800

    拆分用户与会话控制器并适配新API规范

---
+++ src/main/java/top/enderliquid/audioflow/controller/SessionController.java
@@ -0,0 +1,40 @@
+package top.enderliquid.audioflow.controller;
+
+import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.stp.StpUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import top.enderliquid.audioflow.common.response.HttpResponseBody;
+import top.enderliquid.audioflow.dto.request.UserVerifyPasswordDTO;
+import top.enderliquid.audioflow.dto.response.UserVO;
+import top.enderliquid.audioflow.service.UserService;
+
+@RestController
+@RequestMapping("/api/sessions")
+@Validated
+public class SessionController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 用户登录
+     */
+    @PostMapping
+    public HttpResponseBody<UserVO> login(@RequestBody UserVerifyPasswordDTO dto) {
+        UserVO userVO = userService.verifyUserPassword(dto);
+        StpUtil.login(userVO.getId());
+        return HttpResponseBody.ok(userVO, "登录成功");
+    }
+
+    /**
+     * 用户注销（仅当前设备）
+     */
+    @SaCheckLogin
+    @DeleteMapping("current")
+    public HttpResponseBody<Void> logout() {
+        StpUtil.logoutByTokenValue(StpUtil.getTokenValue());
+        return HttpResponseBody.ok(null, "注销成功");
+    }
+}
--- src/main/java/top/enderliquid/audioflow/controller/UserController.java
+++ src/main/java/top/enderliquid/audioflow/controller/UserController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.*;
 import top.enderliquid.audioflow.common.response.HttpResponseBody;
 import top.enderliquid.audioflow.dto.request.UserSaveDTO;
 import top.enderliquid.audioflow.dto.request.UserUpdatePasswordDTO;
-import top.enderliquid.audioflow.dto.request.UserVerifyPasswordDTO;
 import top.enderliquid.audioflow.dto.response.UserVO;
+
 import top.enderliquid.audioflow.service.UserService;
 
 @RestController
-@RequestMapping("/api/user/")
+@RequestMapping("/api/users")
 @Validated
 public class UserController {
 
@@ -23,7 +23,7 @@ public class UserController {
     /**
      * 用户注册
      */
-    @PostMapping("register")
+    @PostMapping
     public HttpResponseBody<UserVO> register(@RequestBody UserSaveDTO dto) {
         UserVO userVO = userService.saveUser(dto);
         StpUtil.login(userVO.getId());
@@ -31,30 +31,10 @@ public class UserController {
     }
 
     /**
-     * 用户登录
-     */
-    @PostMapping("login")
-    public HttpResponseBody<UserVO> login(@RequestBody UserVerifyPasswordDTO dto) {
-        UserVO userVO = userService.verifyUserPassword(dto);
-        StpUtil.login(userVO.getId());
-        return HttpResponseBody.ok(userVO, "登录成功");
-    }
-
-    /**
-     * 用户注销（仅当前设备）
-     */
-    @SaCheckLogin
-    @PostMapping("logout")
-    public HttpResponseBody<Void> logout() {
-        StpUtil.logoutByTokenValue(StpUtil.getTokenValue());
-        return HttpResponseBody.ok(null, "注销成功");
-    }
-
-    /**
      * 获取当前登录用户信息
      */
     @SaCheckLogin
-    @GetMapping("info")
+    @GetMapping("me")
     public HttpResponseBody<UserVO> getUserInfo() {
         long userId = StpUtil.getLoginIdAsLong();
         UserVO userVO = userService.getUser(userId);
@@ -65,11 +45,12 @@ public class UserController {
      * 更改用户密码
      */
     @SaCheckLogin
-    @PostMapping("update/password")
-    public HttpResponseBody<UserVO> changePassword(UserUpdatePasswordDTO dto) {
+    @PatchMapping("me/password")
+    public HttpResponseBody<UserVO> changePassword(@RequestBody UserUpdatePasswordDTO dto) {
         long userId = StpUtil.getLoginIdAsLong();
         UserVO userVO = userService.updateUserPassword(dto, userId);
         StpUtil.kickout(userId);
         return HttpResponseBody.ok(userVO, "密码修改成功，请重新登录");
     }
 }
+

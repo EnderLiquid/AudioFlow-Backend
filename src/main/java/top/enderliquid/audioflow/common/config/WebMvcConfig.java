91a5ffafb290135124c46f18f04e91fc0e9669ec
EnderLiquid <3546817839@qq.com>
2026-02-25 01:02:15 +0800

    删除已失效的静态资源映射

--- src/main/java/top/enderliquid/audioflow/common/config/WebMvcConfig.java
+++ src/main/java/top/enderliquid/audioflow/common/config/WebMvcConfig.java
@@ -1,28 +1,16 @@
 package top.enderliquid.audioflow.common.config;
 
 import cn.dev33.satoken.interceptor.SaInterceptor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import top.enderliquid.audioflow.common.filter.HttpMethodOverrideFilter;
 
 @Configuration
-// 配置静态资源映射
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Value("${file.storage.local.storage-dir}")
-    private String localStorageDir;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/file/**") // 网页url后缀
-                .addResourceLocations("file:" + localStorageDir); // 磁盘路径
-    }
-
     @Bean
     public FilterRegistrationBean<HttpMethodOverrideFilter> httpMethodOverrideFilterRegistration() {
         FilterRegistrationBean<HttpMethodOverrideFilter> registration = new FilterRegistrationBean<>(new HttpMethodOverrideFilter());

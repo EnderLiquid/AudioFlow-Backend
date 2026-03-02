97ddccd023d179ed375fe1ae6cc9761f3f797c1b
EnderLiquid <3546817839@qq.com>
2026-03-03 00:08:38 +0800

    新增文件大小限制

--- src/main/java/top/enderliquid/audioflow/dto/request/song/SongPrepareUploadDTO.java
+++ src/main/java/top/enderliquid/audioflow/dto/request/song/SongPrepareUploadDTO.java
@@ -1,6 +1,8 @@
 package top.enderliquid.audioflow.dto.request.song;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +14,10 @@ import lombok.NoArgsConstructor;
 public class SongPrepareUploadDTO {
     @NotBlank(message = "文件类型不能为空")
     private String mimeType;
+
+    @NotNull(message = "文件大小不能为空")
+    @Min(value = 1, message = "文件大小必须大于0")
+    private Long size;
 
     @NotBlank(message = "歌曲名称不能为空")
     @Size(min = 1, max = 64, message = "歌曲名称长度必须在1-64个字符之间")

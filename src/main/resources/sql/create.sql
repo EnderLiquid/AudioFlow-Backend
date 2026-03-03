34eaf9ebc26509a5aaa0804b42e8f61f35eeb3ac
EnderLiquid <3546817839@qq.com>
2026-03-04 00:15:42 +0800

    实现积分功能

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `user` (
     `name` VARCHAR(255) NOT NULL COMMENT '用户名',
     `password` VARCHAR(255) NOT NULL COMMENT '密码',
     `role` VARCHAR(20) NOT NULL DEFAULT 'USER' COMMENT '角色: USER, ADMIN',
+    `points` INT NOT NULL DEFAULT 100 COMMENT '积分余额',
     `create_time` DATETIME NOT NULL COMMENT '创建时间',
     `update_time` DATETIME NOT NULL COMMENT '更新时间',
     PRIMARY KEY (`id`),

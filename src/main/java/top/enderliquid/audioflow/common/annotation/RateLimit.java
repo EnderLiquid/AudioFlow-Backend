872854ee28b7327190869e06215aa9daad428617
EnderLiquid <3546817839@qq.com>
2026-03-06 19:53:14 +0800

    重构 RateLimit 注解为单一规则注解

--- src/main/java/top/enderliquid/audioflow/common/annotation/RateLimit.java
+++ src/main/java/top/enderliquid/audioflow/common/annotation/RateLimit.java
@@ -5,21 +5,30 @@ import top.enderliquid.audioflow.common.enums.LimitType;
 import java.lang.annotation.*;
 
 /**
- * 限流注解 - 基于令牌桶算法的API限流配置
+ * 单一限流规则注解 - 只能在 @RateLimits 容器注解内使用
  */
 @Documented
-@Target({ElementType.METHOD})
+@Target({})  // 空目标，表示只能在容器注解内使用
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RateLimit {
+    /**
+     * 限流类型：IP / USER / GLOBAL
+     */
+    LimitType type();
+    
+    /**
+     * 令牌补充速率，格式为 "分子/分母"，表示每分母秒补充分子个令牌
+     * 例如 "5/1" 表示每秒补充 5 个令牌
+     */
     String refillRate() default "5/1";
-
+    
+    /**
+     * 令牌桶容量
+     */
     int capacity() default 10;
-
-    LimitType limitType() default LimitType.BOTH;
-
-    String message() default "请求过于频繁，请稍后再试";
-
-    String entryKey() default "";
-
+    
+    /**
+     * 每次请求消耗的令牌数
+     */
     int tokensRequested() default 1;
 }

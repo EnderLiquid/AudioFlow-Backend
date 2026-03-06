ab770df74b0fe86de5c1a416eeb16db3e350636e
EnderLiquid <3546817839@qq.com>
2026-03-06 19:54:43 +0800

    修改 RateLimitService 接口方法签名

--- src/main/java/top/enderliquid/audioflow/service/RateLimitService.java
+++ src/main/java/top/enderliquid/audioflow/service/RateLimitService.java
@@ -1,10 +1,8 @@
 package top.enderliquid.audioflow.service;
 
 import jakarta.validation.constraints.NotNull;
-import org.springframework.validation.annotation.Validated;
 import top.enderliquid.audioflow.common.annotation.RateLimit;
 
-@Validated
 public interface RateLimitService {
-    void verifyRateLimit(@NotNull RateLimit rateLimit, @NotNull String ip, Long userId, @NotNull String methodName);
+    void verifyRateLimit(@NotNull RateLimit limit, @NotNull String ip, Long userId, @NotNull String entry, @NotNull String message);
 }

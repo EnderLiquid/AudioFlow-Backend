73abc147bcac7d5ac111a9b68405f5048cffc7e8
EnderLiquid <3546817839@qq.com>
2026-03-06 19:58:21 +0800

    迁移所有 Controller 到新限流注解格式

--- src/main/java/top/enderliquid/audioflow/service/RateLimitService.java
+++ src/main/java/top/enderliquid/audioflow/service/RateLimitService.java
@@ -1,8 +1,10 @@
 package top.enderliquid.audioflow.service;
 
 import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 import top.enderliquid.audioflow.common.annotation.RateLimit;
 
+@Validated
 public interface RateLimitService {
     void verifyRateLimit(@NotNull RateLimit limit, @NotNull String ip, Long userId, @NotNull String entry, @NotNull String message);
 }

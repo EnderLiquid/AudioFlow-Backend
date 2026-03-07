a9aa12c10b38526f85579b4945e012dcd7b35cd3
EnderLiquid <3546817839@qq.com>
2026-03-07 22:05:14 +0800

    移除手动设置更新时间逻辑

--- src/main/java/top/enderliquid/audioflow/mapper/UserMapper.java
+++ src/main/java/top/enderliquid/audioflow/mapper/UserMapper.java
@@ -8,6 +8,6 @@ import top.enderliquid.audioflow.entity.User;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
-    @Update("UPDATE user SET points = points + #{delta}, update_time = NOW() WHERE id = #{userId} AND points + #{delta} >= 0")
+    @Update("UPDATE user SET points = points + #{delta} WHERE id = #{userId} AND points + #{delta} >= 0")
     int addPoints(@Param("userId") Long userId, @Param("delta") int delta);
 }

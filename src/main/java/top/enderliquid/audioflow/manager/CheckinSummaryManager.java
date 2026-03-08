fe46ddd24398fbe2dd66f0d5e46b0f82627763ad
EnderLiquid <3546817839@qq.com>
2026-03-08 12:08:27 +0800

    新增签到Manager层

---
+++ src/main/java/top/enderliquid/audioflow/manager/CheckinSummaryManager.java
@@ -0,0 +1,10 @@
+package top.enderliquid.audioflow.manager;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import top.enderliquid.audioflow.entity.CheckinSummary;
+
+public interface CheckinSummaryManager extends IService<CheckinSummary> {
+    CheckinSummary getByUserId(Long userId);
+
+    boolean existsByUserId(Long userId);
+}

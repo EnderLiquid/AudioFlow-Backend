c92805362aba81fa9c56b95b78e9f1338efb9f0b
EnderLiquid <3546817839@qq.com>
2026-03-08 12:05:06 +0800

    新增签到Mapper接口

---
+++ src/main/java/top/enderliquid/audioflow/mapper/CheckinLogMapper.java
@@ -0,0 +1,9 @@
+package top.enderliquid.audioflow.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.enderliquid.audioflow.entity.CheckinLog;
+
+@Mapper
+public interface CheckinLogMapper extends BaseMapper<CheckinLog> {
+}
---
+++ src/main/java/top/enderliquid/audioflow/mapper/CheckinSummaryMapper.java
@@ -0,0 +1,9 @@
+package top.enderliquid.audioflow.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.enderliquid.audioflow.entity.CheckinSummary;
+
+@Mapper
+public interface CheckinSummaryMapper extends BaseMapper<CheckinSummary> {
+}

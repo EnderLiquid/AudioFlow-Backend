fb4bcf6f3bb67f327636874eb548bccf6a908682
EnderLiquid <3546817839@qq.com>
2026-03-08 13:04:03 +0800

    新增签到DTO

---
+++ src/main/java/top/enderliquid/audioflow/dto/response/checkin/CheckinStatusVO.java
@@ -0,0 +1,12 @@
+package top.enderliquid.audioflow.dto.response.checkin;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckinStatusVO {
+    private Boolean checkedIn;
+}

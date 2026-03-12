52b91c6668d6840715603e22a381b3fe929c3764
EnderLiquid <3546817839@qq.com>
2026-03-12 21:06:26 +0800

    删除权限获取逻辑

--- src/main/java/top/enderliquid/audioflow/service/impl/StpInterfaceImpl.java
+++ src/main/java/top/enderliquid/audioflow/service/impl/StpInterfaceImpl.java
@@ -1,19 +1,12 @@
 package top.enderliquid.audioflow.service.impl;
 
 import cn.dev33.satoken.stp.StpInterface;
-import org.springframework.beans.factory.annotation.Autowired;
-import top.enderliquid.audioflow.entity.User;
-import top.enderliquid.audioflow.manager.UserManager;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 // 实现StpInterface接口，用于SaToken权限/角色获取
 public class StpInterfaceImpl implements StpInterface {
-    @Autowired
-    private UserManager userManager;
-
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
         return new ArrayList<>();
@@ -21,9 +14,6 @@ public class StpInterfaceImpl implements StpInterface {
 
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
-        long userId = Long.parseLong(loginId.toString());
-        User user = userManager.getById(userId);
-        if (user == null) return new ArrayList<>();
-        return Collections.singletonList(user.getRole().name());
+        return new ArrayList<>();
     }
 }

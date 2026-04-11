7c55b6f092304a98fa1198afc47f865597a5cd3f
EnderLiquid <3546817839@qq.com>
2026-04-12 02:53:34 +0800

    新增 Nullable 注解

--- src/main/java/top/enderliquid/audioflow/dto/bo/SongPageParam.java
+++ src/main/java/top/enderliquid/audioflow/dto/bo/SongPageParam.java
@@ -3,6 +3,7 @@ package top.enderliquid.audioflow.dto.bo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 
 /**
  * 歌曲分页查询参数包装类
@@ -15,21 +16,25 @@ public class SongPageParam {
     /**
      * 上传者关键字（用户名模糊匹配）
      */
+    @Nullable
     private String uploaderKeyword;
 
     /**
      * 上传者ID（精确匹配）
      */
+    @Nullable
     private Long uploaderId;
 
     /**
      * 歌曲关键字（歌名模糊匹配）
      */
+    @Nullable
     private String songKeyword;
 
     /**
      * 歌曲ID（精确匹配）
      */
+    @Nullable
     private Long songId;
 
     /**
